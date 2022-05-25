60bcfceea4cfbea271381709543aedae196c75a6
Oleg Makarov <macosib@yandex.ru>
2022-05-25 22:54:09 +0700

    fix views.bus_stations, add _get_content

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,29 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.conf import settings
+import csv
 
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
+CONTENT_BUS = []
+
+def _get_content():
+    with open(settings.BUS_STATION_CSV, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=",")
+        for row in reader:
+            CONTENT_BUS.append(row)
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
-
+    page_number = request.GET.get('page', 1)
+    if not CONTENT_BUS:
+        _get_content()
+    paginator = Paginator(CONTENT_BUS, 10)
+    page = paginator.get_page(page_number)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+            'bus_stations': page.object_list,
+            'page': page,
     }
     return render(request, 'stations/index.html', context)
