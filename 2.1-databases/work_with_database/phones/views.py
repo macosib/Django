05f2244254a03191e6d78c911adbda57b6e71df4
Oleg Makarov <macosib@yandex.ru>
2022-05-26 17:52:08 +0700

    fix import_phones.handle, add views, add models

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render, redirect
-
+from .models import Phone
 
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
+    sort_param = request.GET.get('sort')
+    sort_dict = {'name': 'name', 'min_price': 'price', 'max_price': "-price"}
     template = 'catalog.html'
-    context = {}
+    phones = Phone.objects.all().order_by(sort_dict.get(sort_param))
+    context = {
+        'phones': phones
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.filter(slug=slug)[0]
+    context = {
+        'phone': phone
+    }
     return render(request, template, context)
