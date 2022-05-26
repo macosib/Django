1c4ce24f0936aa903c4136dd36e4c487f9126164
Oleg Makarov <macosib@yandex.ru>
2022-05-26 17:53:14 +0700

    Revert "fix import_phones.handle, add views, add models"
    This reverts commit 05f2244254a03191e6d78c911adbda57b6e71df4.

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -7,7 +7,6 @@ from phones.models import Phone
 class Command(BaseCommand):
 
     help = 'Fill in the database Phone'
-    version = '1.0'
 
     def add_arguments(self, parser):
         pass
@@ -15,10 +14,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
+        print(phones)
         for phone in phones:
-            Phone(id=phone.get('id'),
-                  name=phone.get('name'),
-                  image=phone.get('image'),
-                  price=phone.get('price'),
-                  release_date=phone.get('release_date'),
-                  lte_exists=phone.get('lte_exists'),).save()
+            # TODO: Добавьте сохранение модели
+            pass
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,25 +1,17 @@
 from django.shortcuts import render, redirect
-from .models import Phone
+
 
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
-    sort_param = request.GET.get('sort')
-    sort_dict = {'name': 'name', 'min_price': 'price', 'max_price': "-price"}
     template = 'catalog.html'
-    phones = Phone.objects.all().order_by(sort_dict.get(sort_param))
-    context = {
-        'phones': phones
-    }
+    context = {}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    phone = Phone.objects.filter(slug=slug)[0]
-    context = {
-        'phone': phone
-    }
+    context = {}
     return render(request, template, context)
