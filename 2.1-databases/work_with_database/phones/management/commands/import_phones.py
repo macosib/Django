da3f817896f2bbb364211e093739bb5dd217a250
Oleg Makarov <macosib@yandex.ru>
2022-05-28 20:23:52 +0700

    Fix import_phones.py

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -7,6 +7,7 @@ from phones.models import Phone
 class Command(BaseCommand):
 
     help = 'Fill in the database Phone'
+    version = '1.0'
 
     def add_arguments(self, parser):
         pass
@@ -14,7 +15,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
-        print(phones)
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            Phone.objects.create(id=phone.get('id'),
+                  name=phone.get('name'),
+                  image=phone.get('image'),
+                  price=phone.get('price'),
+                  release_date=phone.get('release_date'),
+                  lte_exists=phone.get('lte_exists'),)
