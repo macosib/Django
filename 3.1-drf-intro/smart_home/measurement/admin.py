31437c318a67ca69040bb09bd1ac34fbb98ed3f4
Oleg Makarov <macosib@yandex.ru>
2022-05-31 16:13:32 +0700

    add smart_home

--- 3.1-drf-intro/smart_home/measurement/admin.py
+++ 3.1-drf-intro/smart_home/measurement/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Sensor, Measurement
 
 # Register your models here.
+
+@admin.register(Sensor)
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description']
+    ordering = ['id']
+
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'sensor', 'temperature', 'created_at']
+    ordering = ['id']
--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -1,3 +1,17 @@
 from rest_framework import serializers
 
-# TODO: опишите необходимые сериализаторы
+from measurement.models import Sensor, Measurement
+
+
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ['sensor', 'temperature', 'created_at']
+
+
+class SensorSerializer(serializers.ModelSerializer):
+    measurements = MeasurementSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description', 'measurements']
